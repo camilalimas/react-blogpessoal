9fa8d7380ce2b1a8fa390ca15fc9d06dc63b26c8
camilalimas <camila.lsw@gmail.com>
2025-06-02 21:48:43 -0300

    Deploy v2

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
